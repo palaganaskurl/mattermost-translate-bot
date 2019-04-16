980ceb22a1155e47592188e15eb5b5e77a2a7612
Kurl Angelo Palaganas <palaganaskurl@gmail.com>
2019-04-16 10:38:52 +0800

    added webhook response

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('11');
+  res.send('Test!');
 });
 
 app.post('/webhook', (req, res) => {
@@ -14,6 +14,11 @@ app.post('/webhook', (req, res) => {
 
   console.log(req.body);
   console.log(text);
+
+  res.set('Content-Type', 'application/json');
+  res.status(200).send({
+    text: 'Translate.'
+  });
 });
 
 const listener = app.listen(port, () => console.log(`App is listening on port ${listener.address().port}`));
